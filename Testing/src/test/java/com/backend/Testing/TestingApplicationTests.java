efb2eeb76adf2022cc997c4709ee6a60d6b38b33
krishsharma955 <krishsharma9005@gmail.com>
2026-07-11 20:02:49 +0530

    feat: Assertions (JUnit)

--- Testing/src/test/java/com/backend/Testing/TestingApplicationTests.java
+++ Testing/src/test/java/com/backend/Testing/TestingApplicationTests.java
@@ -6,36 +6,23 @@ import org.junit.jupiter.api.*;
 @Slf4j
 class TestingApplicationTests {
 
-    @BeforeEach
-    void setupConfig() {
-        log.info("Setting up Config");
-    }
-
-    @AfterEach
-    void testExecuted() {
-        log.info("Test case successfully executed");
-    }
-
-    @BeforeAll
-    static void configInit() {
-        log.info("Initial Commit");
-    }
-
-    @AfterAll
-    static void testEnd() {
-        log.info("Final Commit");
-    }
-
 	@Test
-//    @Disabled
 	void testNumberOne() {
-        log.info("test one is running");
+        int a = 5, b = 3;
+        int res = add2Numbers(a, b);
+
+        Assertions.assertEquals(8, res); //checks the expected result with evaluated result
 	}
 
     @Test
-//    @DisplayName("Test Case 2")
     void testNumberTwo() {
-        log.info("test two is running");
+
+    }
+
+    /// Utility Methods
+
+    int add2Numbers(int a, int b) {
+        return a + b;
     }
 
 }
