7ec44ce436f579ef7c56bac1232f577ed47e008f
krishsharma955 <krishsharma9005@gmail.com>
2026-07-11 20:27:16 +0530

    feat: Assertions (AssertJ)

--- Testing/src/test/java/com/backend/Testing/TestingApplicationTests.java
+++ Testing/src/test/java/com/backend/Testing/TestingApplicationTests.java
@@ -1,7 +1,12 @@
 package com.backend.Testing;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.*;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 @Slf4j
 class TestingApplicationTests {
@@ -11,18 +16,42 @@ class TestingApplicationTests {
         int a = 5, b = 3;
         int res = add2Numbers(a, b);
 
-        Assertions.assertEquals(8, res); //checks the expected result with evaluated result
+        assertThat(res)
+                .isEqualTo(8)
+                .isCloseTo(9, Offset.offset(1));
 	}
 
     @Test
     void testNumberTwo() {
+        assertThat("apple")
+                .isEqualTo("apple")
+                .startsWith("app")
+                .endsWith("le")
+                .hasSize(5);
+    }
 
+    @Test
+    void testDivideTwoNumbers_whenDenominatorIsZero_ThenArithmeticException() { //exception checking
+        int a = 5, b = 0;
+
+        assertThatThrownBy(() -> divide2Numbers(a, b))
+                .isInstanceOf(ArithmeticException.class)
+                .hasMessage("Tried to divide by Zero");
     }
 
     /// Utility Methods
 
     int add2Numbers(int a, int b) {
         return a + b;
+    }
+
+    int divide2Numbers(int a, int b) {
+        try {
+            return a / b;
+        } catch (ArithmeticException e) {
+            log.error("Arithmetic Exception occurred: {}", e.getLocalizedMessage());
+            throw new ArithmeticException("Tried to divide by Zero");
+        }
     }
 
 }
